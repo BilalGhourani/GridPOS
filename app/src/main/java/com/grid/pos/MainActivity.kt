c7b8d181e3614ea83e14522b65c2f701ee88554d
bilalghourani <bilalghourani92@gmail.com>
2024-05-01 19:33:03 +0300

    start with LoginView

--- app/src/main/java/com/grid/pos/MainActivity.kt
+++ app/src/main/java/com/grid/pos/MainActivity.kt
@@ -36,7 +36,7 @@ class MainActivity : ComponentActivity() {
                 ) {
                     AuthNavGraph(
                         navController = navController,
-                        startDestination = "HomeView"
+                        startDestination = "LoginView"
                     )
                 }
             }
