3ae074ad5bd1802204a1e34c7cba31eb8f359a43
bilalghourani <bilalghourani92@gmail.com>
2024-07-04 07:53:31 +0300

    handle  pos receipt in sql server

--- app/src/main/java/com/grid/pos/data/PosReceipt/PosReceipt.kt
+++ app/src/main/java/com/grid/pos/data/PosReceipt/PosReceipt.kt
@@ -17,100 +17,122 @@ import java.util.Date
 @Entity(tableName = "pos_receipt")
 data class PosReceipt(
         /**
-     * POS Receipt Id
-     * */
-    @PrimaryKey
-    @ColumnInfo(name = "pr_id")
-    @set:PropertyName("pr_id")
-    @get:PropertyName("pr_id")
-    var posReceiptId: String,
+         * POS Receipt Id
+         * */
+        @PrimaryKey
+        @ColumnInfo(name = "pr_id")
+        @set:PropertyName("pr_id")
+        @get:PropertyName("pr_id")
+        var posReceiptId: String,
 
         @Ignore
-    @get:Exclude
-    var posReceiptDocumentId: String? = null,
+        @get:Exclude
+        var posReceiptDocumentId: String? = null,
 
         /**
-     * related POS Receipt Invoice Id
-     * */
-    @ColumnInfo(name = "pr_hi_id")
-    @set:PropertyName("pr_hi_id")
-    @get:PropertyName("pr_hi_id")
-    var posReceiptInvoiceId: String? = null,
+         * related POS Receipt Invoice Id
+         * */
+        @ColumnInfo(name = "pr_hi_id")
+        @set:PropertyName("pr_hi_id")
+        @get:PropertyName("pr_hi_id")
+        var posReceiptInvoiceId: String? = null,
+
+        @Ignore
+        @get:Exclude
+        var posReceiptCashID: String? = null,
 
         /**
-     *  POS Receipt Cash Amount
-     * */
-    @ColumnInfo(name = "pr_cash")
-    @set:PropertyName("pr_cash")
-    @get:PropertyName("pr_cash")
-    var posReceiptCash: Double = 0.0,
+         *  POS Receipt Cash Amount
+         * */
+        @ColumnInfo(name = "pr_cash")
+        @set:PropertyName("pr_cash")
+        @get:PropertyName("pr_cash")
+        var posReceiptCash: Double = 0.0,
+
+        @Ignore
+        @get:Exclude
+        var posReceiptCashsID: String? = null,
 
         /**
-     *  POS Receipt Cash Amount 2
-     * */
-    @ColumnInfo(name = "pr_cashs")
-    @set:PropertyName("pr_cashs")
-    @get:PropertyName("pr_cashs")
-    var posReceiptCashs: Double = 0.0,
+         *  POS Receipt Cash Amount 2
+         * */
+        @ColumnInfo(name = "pr_cashs")
+        @set:PropertyName("pr_cashs")
+        @get:PropertyName("pr_cashs")
+        var posReceiptCashs: Double = 0.0,
+
+        @Ignore
+        @get:Exclude
+        var posReceiptCreditID: String? = null,
 
         /**
-     *  POS Receipt Credit Amount
-     * */
-    @ColumnInfo(name = "pr_credit")
-    @set:PropertyName("pr_credit")
-    @get:PropertyName("pr_credit")
-    var posReceiptCredit: Double = 0.0,
+         *  POS Receipt Credit Amount
+         * */
+        @ColumnInfo(name = "pr_credit")
+        @set:PropertyName("pr_credit")
+        @get:PropertyName("pr_credit")
+        var posReceiptCredit: Double = 0.0,
+
+        @Ignore
+        @get:Exclude
+        var posReceiptCreditsID: String? = null,
 
         /**
-     *  POS Receipt Credit Amounts
-     * */
-    @ColumnInfo(name = "pr_credits")
-    @set:PropertyName("pr_credits")
-    @get:PropertyName("pr_credits")
-    var posReceiptCredits: Double = 0.0,
+         *  POS Receipt Credit Amounts
+         * */
+        @ColumnInfo(name = "pr_credits")
+        @set:PropertyName("pr_credits")
+        @get:PropertyName("pr_credits")
+        var posReceiptCredits: Double = 0.0,
+
+        @Ignore
+        @get:Exclude
+        var posReceiptDebitID: String? = null,
 
         /**
-     *  POS Receipt Debit Amount
-     * */
-    @ColumnInfo(name = "pr_debit")
-    @set:PropertyName("pr_debit")
-    @get:PropertyName("pr_debit")
-    var posReceiptDebit: Double = 0.0,
+         *  POS Receipt Debit Amount
+         * */
+        @ColumnInfo(name = "pr_debit")
+        @set:PropertyName("pr_debit")
+        @get:PropertyName("pr_debit")
+        var posReceiptDebit: Double = 0.0,
+
+        @Ignore
+        @get:Exclude
+        var posReceiptDebitsID: String? = null,
 
         /**
-     *  POS Receipt Debit Amount 2
-     * */
-    @ColumnInfo(name = "pr_debits")
-    @set:PropertyName("pr_debits")
-    @get:PropertyName("pr_debits")
-    var posReceiptDebits: Double = 0.0,
-
-
-        /**
-     *  POS Receipt Date
-     * */
-    @Ignore
-    @set:PropertyName("pr_timestamp")
-    @get:PropertyName("pr_timestamp")
-    @ServerTimestamp
-    var posReceiptTimeStamp: Date? = null,
+         *  POS Receipt Debit Amount 2
+         * */
+        @ColumnInfo(name = "pr_debits")
+        @set:PropertyName("pr_debits")
+        @get:PropertyName("pr_debits")
+        var posReceiptDebits: Double = 0.0,
 
         /**
-     *  POS Receipt Date
-     * */
-    @ColumnInfo(name = "pr_datetime")
-    @set:PropertyName("pr_datetime")
-    @get:PropertyName("pr_datetime")
-    var posReceiptDateTime: Long = System.currentTimeMillis(),
-
+         *  POS Receipt Date
+         * */
+        @Ignore
+        @set:PropertyName("pr_timestamp")
+        @get:PropertyName("pr_timestamp")
+        @ServerTimestamp
+        var posReceiptTimeStamp: Date? = null,
 
         /**
-     *  POS Receipt UserStamp
-     * */
-    @ColumnInfo(name = "pr_userstamp")
-    @set:PropertyName("pr_userstamp")
-    @get:PropertyName("pr_userstamp")
-    var posReceiptUserStamp: String? = null,
+         *  POS Receipt Date
+         * */
+        @ColumnInfo(name = "pr_datetime")
+        @set:PropertyName("pr_datetime")
+        @get:PropertyName("pr_datetime")
+        var posReceiptDateTime: Long = System.currentTimeMillis(),
+
+        /**
+         *  POS Receipt UserStamp
+         * */
+        @ColumnInfo(name = "pr_userstamp")
+        @set:PropertyName("pr_userstamp")
+        @get:PropertyName("pr_userstamp")
+        var posReceiptUserStamp: String? = null,
 ) : DataModel() {
     constructor() : this("")
 
--- app/src/main/java/com/grid/pos/data/PosReceipt/PosReceiptRepositoryImpl.kt
+++ app/src/main/java/com/grid/pos/data/PosReceipt/PosReceiptRepositoryImpl.kt
@@ -6,6 +6,7 @@ import com.grid.pos.data.SQLServerWrapper
 import com.grid.pos.model.CONNECTION_TYPE
 import com.grid.pos.model.SettingsModel
 import com.grid.pos.utils.DateHelper
+import com.grid.pos.utils.Utils
 import kotlinx.coroutines.tasks.await
 import org.json.JSONObject
 import java.util.Date
@@ -28,11 +29,13 @@ class PosReceiptRepositoryImpl(
             }
 
             else -> {
-                SQLServerWrapper.insert(
-                    "pos_receipt",
-                    getColumns(),
-                    getValues(posReceipt)
-                )
+                val rate = SettingsModel.currentCurrency?.currencyRate ?: 1.0
+                SQLServerWrapper.insert("pos_receipt", getColumns(), getValues(Utils.generateRandomUuidString(),posReceipt,0,rate))
+                SQLServerWrapper.insert("pos_receipt", getColumns(), getValues(Utils.generateRandomUuidString(),posReceipt,1,rate))
+                SQLServerWrapper.insert("pos_receipt", getColumns(), getValues(Utils.generateRandomUuidString(),posReceipt,2,rate))
+                SQLServerWrapper.insert("pos_receipt", getColumns(), getValues(Utils.generateRandomUuidString(),posReceipt,3,rate))
+                SQLServerWrapper.insert("pos_receipt", getColumns(), getValues(Utils.generateRandomUuidString(),posReceipt,4,rate))
+                SQLServerWrapper.insert("pos_receipt", getColumns(), getValues(Utils.generateRandomUuidString(),posReceipt,5,rate))
             }
         }
         return posReceipt
@@ -54,10 +57,12 @@ class PosReceiptRepositoryImpl(
             }
 
             else -> {
-                SQLServerWrapper.delete(
-                    "pos_receipt",
-                    "pr_id = ${posReceipt.posReceiptId}"
-                )
+                SQLServerWrapper.delete("pos_receipt", "pr_id = ${posReceipt.posReceiptCashID}")
+                SQLServerWrapper.delete("pos_receipt", "pr_id = ${posReceipt.posReceiptCashsID}")
+                SQLServerWrapper.delete("pos_receipt", "pr_id = ${posReceipt.posReceiptCreditID}")
+                SQLServerWrapper.delete("pos_receipt", "pr_id = ${posReceipt.posReceiptCreditsID}")
+                SQLServerWrapper.delete("pos_receipt", "pr_id = ${posReceipt.posReceiptDebitID}")
+                SQLServerWrapper.delete("pos_receipt", "pr_id = ${posReceipt.posReceiptDebitsID}")
             }
         }
     }
@@ -78,12 +83,26 @@ class PosReceiptRepositoryImpl(
             }
 
             else -> {
-                SQLServerWrapper.update(
-                    "pos_receipt",
-                    getColumns(),
-                    getValues(posReceipt),
-                    "pr_id = ${posReceipt.posReceiptId}"
-                )
+                val rate = SettingsModel.currentCurrency?.currencyRate ?: 1.0
+                if(!posReceipt.posReceiptCashID.isNullOrEmpty()){
+                    SQLServerWrapper.update("pos_receipt", getColumns(), getValues(posReceipt.posReceiptCashID!!,posReceipt,0,rate), "pr_id = ${posReceipt.posReceiptCashID!!}")
+                }
+                if(!posReceipt.posReceiptCashsID.isNullOrEmpty()){
+                    SQLServerWrapper.update("pos_receipt", getColumns(), getValues(posReceipt.posReceiptCashsID!!,posReceipt,1,rate), "pr_id = ${posReceipt.posReceiptCashsID!!}")
+                }
+                if(!posReceipt.posReceiptCreditID.isNullOrEmpty()){
+                    SQLServerWrapper.update("pos_receipt", getColumns(), getValues(posReceipt.posReceiptCreditID!!,posReceipt,2,rate), "pr_id = ${posReceipt.posReceiptCreditID!!}")
+                }
+                if(!posReceipt.posReceiptCreditsID.isNullOrEmpty()){
+                    SQLServerWrapper.update("pos_receipt", getColumns(), getValues(posReceipt.posReceiptCreditsID!!,posReceipt,3,rate), "pr_id = ${posReceipt.posReceiptCreditsID!!}")
+                }
+                if(!posReceipt.posReceiptDebitID.isNullOrEmpty()){
+                    SQLServerWrapper.update("pos_receipt", getColumns(), getValues(posReceipt.posReceiptDebitID!!,posReceipt,4,rate), "pr_id = ${posReceipt.posReceiptDebitID!!}")
+                }
+                if(!posReceipt.posReceiptDebitsID.isNullOrEmpty()){
+                    SQLServerWrapper.update("pos_receipt", getColumns(), getValues(posReceipt.posReceiptDebitsID!!,posReceipt,5,rate), "pr_id = ${posReceipt.posReceiptDebitsID!!}")
+                }
+
             }
         }
     }
@@ -120,31 +139,59 @@ class PosReceiptRepositoryImpl(
                     mutableListOf("*"),
                     where
                 )
-                val posReceipts: MutableList<PosReceipt> = mutableListOf()
-                dbResult.forEach { obj ->
-                    posReceipts.add(fillParams(obj))
-                }
-                return if (posReceipts.size > 0) posReceipts[0] else null
-            }
-        }
-    }
+                if (dbResult.isNotEmpty()) {
+                    val posReceipt = PosReceipt()
+                    var commonAreFilled = false
+                    dbResult.forEachIndexed { index, obj ->
+                        if (!commonAreFilled) {
+                            commonAreFilled = true
+                            posReceipt.posReceiptId = obj.optString("pr_id")
+                            posReceipt.posReceiptInvoiceId = obj.optString("pr_hi_id")
 
-    private fun fillParams(obj: JSONObject): PosReceipt {
-        return PosReceipt().apply {
-            posReceiptId = obj.optString("pr_id")
-            posReceiptInvoiceId = obj.optString("pr_hi_id")
-            posReceiptCash = obj.optDouble("pr_amt")
-            posReceiptCashs = obj.optDouble("pr_amtf")/*posReceiptCredit = obj.optDouble("pr_amtf")
-                        posReceiptCzredits = obj.optDouble("pr_amtf")
-                        posReceiptDebit = obj.optDouble("pr_amtf")
-                        posReceiptDebits = obj.optDouble("pr_amtf")*/
-            val timeStamp = obj.opt("pr_timestamp")
-            posReceiptTimeStamp = if (timeStamp is Date) timeStamp else DateHelper.getDateFromString(
-                timeStamp as String,
-                "yyyy-MM-dd hh:mm:ss.SSS"
-            )
-            posReceiptDateTime = posReceiptTimeStamp!!.time
-            posReceiptUserStamp = obj.optString("pr_userstamp")
+                            val timeStamp = obj.opt("pr_timestamp")
+                            posReceipt.posReceiptTimeStamp = if (timeStamp is Date) timeStamp else DateHelper.getDateFromString(
+                                timeStamp as String,
+                                "yyyy-MM-dd hh:mm:ss.SSS"
+                            )
+                            posReceipt.posReceiptDateTime = posReceipt.posReceiptTimeStamp!!.time
+                            posReceipt.posReceiptUserStamp = obj.optString("pr_userstamp")
+                        }
+                        when (index) {
+                            0 -> {
+                                posReceipt.posReceiptCashID = obj.optString("pr_id")
+                                posReceipt.posReceiptCash = obj.optDouble("pr_amt")
+                            }
+
+                            1 -> {
+                                posReceipt.posReceiptCashsID = obj.optString("pr_id")
+                                posReceipt.posReceiptCashs = obj.optDouble("pr_amt")
+                            }
+
+                            2 -> {
+                                posReceipt.posReceiptCreditID = obj.optString("pr_id")
+                                posReceipt.posReceiptCredit = obj.optDouble("pr_amt")
+                            }
+
+                            3 -> {
+                                posReceipt.posReceiptCreditsID = obj.optString("pr_id")
+                                posReceipt.posReceiptCredits = obj.optDouble("pr_amt")
+                            }
+
+                            4 -> {
+                                posReceipt.posReceiptDebitID = obj.optString("pr_id")
+                                posReceipt.posReceiptDebit = obj.optDouble("pr_amt")
+                            }
+
+                            5 -> {
+                                posReceipt.posReceiptDebitsID = obj.optString("pr_id")
+                                posReceipt.posReceiptDebits = obj.optDouble("pr_amt")
+                            }
+                        }
+                    }
+                    return posReceipt
+                }
+                return null
+            }
         }
     }
 
@@ -154,20 +201,87 @@ class PosReceiptRepositoryImpl(
             "pr_hi_id",
             "pr_amt",
             "pr_amtf",
+            "pr_amts",
+            "pr_amtinvcurr",
             "pr_timestamp",
             "pr_userstamp",
         )
     }
 
-    private fun getValues(posReceipt: PosReceipt): List<Any?> {
-        return listOf(
-            posReceipt.posReceiptId,
-            posReceipt.posReceiptInvoiceId,
-            posReceipt.posReceiptCash,
-            posReceipt.posReceiptCashs,
-            posReceipt.posReceiptTimeStamp,
-            posReceipt.posReceiptUserStamp,
-        )
+    private fun getValues(
+            id:String,
+            posReceipt: PosReceipt,
+            index: Int,
+            rate : Double
+    ): List<Any?> {
+        return when (index) {
+            0 -> listOf(
+                id,
+                posReceipt.posReceiptInvoiceId,
+                posReceipt.posReceiptCash,
+                posReceipt.posReceiptCash,
+                posReceipt.posReceiptCash.times(rate),
+                posReceipt.posReceiptCash,
+                posReceipt.posReceiptTimeStamp,
+                posReceipt.posReceiptUserStamp,
+            )
+
+            1 -> listOf(
+                id,
+                posReceipt.posReceiptInvoiceId,
+                posReceipt.posReceiptCashs,
+                posReceipt.posReceiptCashs,
+                posReceipt.posReceiptCash.div(rate),
+                posReceipt.posReceiptCashs,
+                posReceipt.posReceiptTimeStamp,
+                posReceipt.posReceiptUserStamp,
+            )
+
+            2 -> listOf(
+                id,
+                posReceipt.posReceiptInvoiceId,
+                posReceipt.posReceiptCredit,
+                posReceipt.posReceiptCredit,
+                posReceipt.posReceiptCredit.times(rate),
+                posReceipt.posReceiptCredit,
+                posReceipt.posReceiptTimeStamp,
+                posReceipt.posReceiptUserStamp,
+            )
+
+            3 -> listOf(
+                id,
+                posReceipt.posReceiptInvoiceId,
+                posReceipt.posReceiptCredits,
+                posReceipt.posReceiptCredits,
+                posReceipt.posReceiptCredits.div(rate),
+                posReceipt.posReceiptCredits,
+                posReceipt.posReceiptTimeStamp,
+                posReceipt.posReceiptUserStamp,
+            )
+
+            4 -> listOf(
+                id,
+                posReceipt.posReceiptInvoiceId,
+                posReceipt.posReceiptDebit,
+                posReceipt.posReceiptDebit,
+                posReceipt.posReceiptDebit.times(rate),
+                posReceipt.posReceiptDebit,
+                posReceipt.posReceiptTimeStamp,
+                posReceipt.posReceiptUserStamp,
+            )
+
+            else -> listOf(
+                id,
+                posReceipt.posReceiptInvoiceId,
+                posReceipt.posReceiptDebits,
+                posReceipt.posReceiptDebits,
+                posReceipt.posReceiptDebits.div(rate),
+                posReceipt.posReceiptDebits,
+                posReceipt.posReceiptTimeStamp,
+                posReceipt.posReceiptUserStamp,
+            )
+
+        }
     }
 
 
