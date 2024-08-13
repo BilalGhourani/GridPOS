b8f6d82103804e999e0cbab8c7d621bfb371bb34
bilalghourani <bilalghourani92@gmail.com>
2024-08-14 00:44:06 +0300

    add BluetoothPrinter class to print on bluetooth printer

---
+++ app/src/main/java/com/grid/pos/utils/BluetoothPrinter.kt
@@ -0,0 +1,76 @@
+package com.grid.pos.utils
+
+import android.Manifest
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothManager
+import android.bluetooth.BluetoothSocket
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.os.Build
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import java.io.IOException
+import java.io.OutputStream
+import java.util.UUID
+
+class BluetoothPrinter {
+
+    private var bluetoothSocket: BluetoothSocket? = null
+    private var outputStream: OutputStream? = null
+
+    fun connectToPrinter(context: Context,printerName: String): Boolean {
+        val bluetoothManager = context.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S && ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.BLUETOOTH_CONNECT
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return false
+        }
+        val device: BluetoothDevice? = bluetoothManager.adapter.bondedDevices.firstOrNull { it.name == printerName }
+
+        if (device == null) {
+            Log.e("BluetoothPrinter", "Printer not found")
+            return false
+        }
+
+        return try {
+            val uuid: UUID = device.uuids[0].uuid
+            bluetoothSocket = device.createRfcommSocketToServiceRecord(uuid)
+            bluetoothSocket?.connect()
+            outputStream = bluetoothSocket?.outputStream
+            true
+        } catch (e: IOException) {
+            Log.e("BluetoothPrinter", "Failed to connect to printer", e)
+            false
+        }
+    }
+
+    fun printData(data: String) {
+        try {
+            outputStream?.write(data.toByteArray())
+            outputStream?.flush()
+        } catch (e: IOException) {
+            Log.e("BluetoothPrinter", "Failed to print data", e)
+        }
+    }
+
+    fun printData(byteArray: ByteArray) {
+        try {
+            outputStream?.write(byteArray)
+            outputStream?.flush()
+        } catch (e: IOException) {
+            Log.e("BluetoothPrinter", "Failed to print data", e)
+        }
+    }
+
+    fun disconnectPrinter() {
+        try {
+            outputStream?.close()
+            bluetoothSocket?.close()
+        } catch (e: IOException) {
+            Log.e("BluetoothPrinter", "Failed to close connection", e)
+        }
+    }
+}
