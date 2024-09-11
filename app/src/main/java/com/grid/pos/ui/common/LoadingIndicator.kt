8bcdc61d7c64bc38ef0068b1038ad4ba4bb2364b
bilalghourani <bilal.ghourani@vianeos.com>
2024-09-11 09:10:28 +0300

    set LoadingIndicator clickable without  click effect

--- app/src/main/java/com/grid/pos/ui/common/LoadingIndicator.kt
+++ app/src/main/java/com/grid/pos/ui/common/LoadingIndicator.kt
@@ -4,27 +4,25 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 @Composable
 fun LoadingIndicator(
-    label: String = "Loading...",
     show: Boolean = true
 ) {
     AnimatedVisibility(
@@ -37,6 +35,11 @@ fun LoadingIndicator(
             Modifier
                 .fillMaxSize()
                 .background(Color.Black.copy(alpha = .6f))
+                .clickable(
+                    indication = null, // Removes the ripple effect
+                    interactionSource = remember { MutableInteractionSource() }, // Required when indication is null
+                    onClick = {}
+                )
         ) {
             Card(
                 Modifier
