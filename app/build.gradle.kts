71f9bf79a6a0fdf41de960db64f410ffba145743
bilalghourani <bilalghourani92@gmail.com>
2024-11-14 22:37:32 +0200

    build 1.0.1(20)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "com.grid.pos"
         minSdk = 26
         targetSdk = 34
-        versionCode = 19
+        versionCode = 20
         versionName = "1.0.1"
         archivesName = "grid_pos_release(${versionName}(${versionCode}))"
 
