53a89f90427f7cf9606027afeeb882f98c3601c8
bilalghourani <bilalghourani92@gmail.com>
2025-02-22 06:08:00 +0200

    fix a query

--- app/src/main/java/com/grid/pos/data/invoiceHeader/InvoiceHeaderDao.kt
+++ app/src/main/java/com/grid/pos/data/invoiceHeader/InvoiceHeaderDao.kt
@@ -36,7 +36,7 @@ interface InvoiceHeaderDao {
     fun getAllInvoiceHeaders(limit:Int,companyId: String): MutableList<InvoiceHeader>
 
     // Get all Invoice Headers as stream.
-    @Query("SELECT * FROM `in_hinvoice` WHERE hi_cmp_id=:companyId AND (hi_transno LIKE '%'|| :key ||'%' OR hi_orderno LIKE '%'|| :key ||'%') ORDER BY hi_datetime DESC LIMIT :limit")
+    @Query("SELECT * FROM `in_hinvoice` WHERE hi_cmp_id=:companyId AND (hi_transno LIKE '%'|| :key ||'%' OR hi_orderno LIKE '%'|| :key ||'%') and ((hi_transno IS NOT NULL AND hi_transno != '') OR ((hi_transno IS NULL OR hi_transno = '') AND hi_ta_name IS NULL)) ORDER BY hi_datetime DESC LIMIT :limit")
     fun getInvoiceHeadersWith(key:String,limit:Int,companyId: String): MutableList<InvoiceHeader>
 
     // Get all Invoice Headers without limit as stream.
