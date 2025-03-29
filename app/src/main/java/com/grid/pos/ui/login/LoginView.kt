f2b58c33e1930fc2b7d5a7ef50f34ba9db3879b0
bilalghourani <bilalghourani92@gmail.com>
2025-03-29 11:10:51 +0200

    login view ui enhancement add register button

--- app/src/main/java/com/grid/pos/ui/login/LoginView.kt
+++ app/src/main/java/com/grid/pos/ui/login/LoginView.kt
@@ -1,11 +1,17 @@
 package com.grid.pos.ui.login
 
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentWidth
@@ -20,6 +26,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -34,7 +41,9 @@ import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -44,12 +53,15 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.grid.pos.App
 import com.grid.pos.R
 import com.grid.pos.model.SettingsModel
 import com.grid.pos.ui.common.UIImageButton
 import com.grid.pos.ui.common.UITextField
 import com.grid.pos.ui.navigation.Screen
 import com.grid.pos.ui.theme.GridPOSTheme
+import com.grid.pos.ui.theme.homeLightBlue
+import com.grid.pos.utils.Constants
 
 @SuppressLint("CoroutineCreationDuringComposition")
 @OptIn(ExperimentalMaterial3Api::class)
@@ -75,27 +87,18 @@ fun LoginView(
         Scaffold(containerColor = SettingsModel.backgroundColor,
             topBar = {
                 Surface(
-                    shadowElevation = 3.dp,
+                    shadowElevation = 0.dp,
                     color = SettingsModel.backgroundColor
                 ) {
                     TopAppBar(colors = TopAppBarDefaults.mediumTopAppBarColors(
                         containerColor = SettingsModel.topBarColor
                     ),
-                        title = {
-                            Text(
-                                text = "Login",
-                                color = SettingsModel.textColor,
-                                fontSize = 16.sp,
-                                textAlign = TextAlign.Center
-                            )
-                        },
+                        title = {},
                         actions = {
-                            IconButton(onClick = { navController?.navigate(Screen.SettingsView.route) }) {
-                                Icon(
-                                    painterResource(R.drawable.ic_settings),
-                                    contentDescription = "Back",
-                                    tint = SettingsModel.buttonColor
-                                )
+                            TextButton(onClick = {
+                                navController?.navigate(Screen.SettingsView.route)
+                            }) {
+                                Text(text = "Settings", color = homeLightBlue)
                             }
                         })
                 }
@@ -108,10 +111,31 @@ fun LoginView(
                     .verticalScroll(rememberScrollState())
             ) {
                 Column(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(24.dp),
+                    verticalArrangement = Arrangement.Center,
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    UITextField(modifier = Modifier.padding(10.dp),
+
+                    Spacer(modifier = Modifier.height(24.dp))
+
+                    // Title
+                    Text(
+                        text = "Grids POS",
+                        textAlign = TextAlign.Center,
+                        style = TextStyle(
+                            fontWeight = FontWeight.Bold,
+                            fontStyle = FontStyle.Italic,
+                            fontSize = 35.sp,
+                            color = homeLightBlue,
+                        ),
+                        modifier = Modifier
+                    )
+
+                    Spacer(modifier = Modifier.height(24.dp))
+
+                    UITextField(modifier = Modifier,
                         defaultValue = viewModel.usernameState.value,
                         label = "Username",
                         placeHolder = "Username",
@@ -119,7 +143,9 @@ fun LoginView(
                         viewModel.usernameState.value = username
                     }
 
-                    UITextField(modifier = Modifier.padding(10.dp),
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    UITextField(modifier = Modifier,
                         defaultValue = viewModel.passwordState.value,
                         label = "Password",
                         placeHolder = "Password",
@@ -144,19 +170,59 @@ fun LoginView(
                         }) { password ->
                         viewModel.passwordState.value = password
                     }
+
+                    Spacer(modifier = Modifier.height(24.dp))
+
                     UIImageButton(
                         modifier = Modifier
                             .wrapContentWidth()
-                            .height(100.dp)
-                            .padding(10.dp),
+                            .height(100.dp),
                         icon = R.drawable.login,
                         text = "Login",
                         iconSize = 60.dp,
                         isVertical = false
                     ) {
                         keyboardController?.hide()
-                        viewModel.login(context){ destination ->
+                        viewModel.login(context) { destination ->
                             navController?.navigate(destination)
+                        }
+                    }
+
+                    if (Constants.PLAY_STORE_VERSION) {
+
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        // Register label
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.Center,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Text(
+                                text = "Don't have an account?",
+                                textAlign = TextAlign.Center,
+                                style = TextStyle(
+                                    fontWeight = FontWeight.Normal,
+                                    fontStyle = FontStyle.Normal,
+                                    fontSize = 16.sp,
+                                    color = SettingsModel.textColor,
+                                )
+                            )
+                            TextButton(onClick = {
+                                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(App.getInstance().getConfigValue("registration_url")))
+                                context.startActivity(intent)
+                            }) {
+                                Text(
+                                    text = "Register",
+                                    textAlign = TextAlign.Center,
+                                    style = TextStyle(
+                                        fontWeight = FontWeight.SemiBold,
+                                        fontStyle = FontStyle.Normal,
+                                        fontSize = 16.sp,
+                                        color = homeLightBlue,
+                                    )
+                                )
+                            }
                         }
                     }
                 }
